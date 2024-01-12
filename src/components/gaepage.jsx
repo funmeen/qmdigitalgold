eb6a02e13de611c8fd83b7dbcf2991706f0e4bba
funmeen <irfanmuhymeen@gmail.com>
2024-01-12 19:38:29 +0800

    touch up update

--- src/components/gaepage.jsx
+++ src/components/gaepage.jsx
@@ -1,34 +1,96 @@
+import React, { useState, useEffect } from 'react';
 import UserInput from "./userinput";
 
-export default function GAEPage({ onBackClick, userInput, handleChange }) {
-    return (
-      <div className='p-4'>
-        <div className='flex flex-wrap'>
-          <div className='p-4 w-full md:w-1/3 md:order-2 '>
-            <div className='p-7 border rounded-md bg-yellow-300'>
-              <p>GOLD ASSET ENHANCEMENT</p>
-              <br></br>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                when an unknown printer took a galley of type and scrambled it to make a type 
-                specimen book. It has survived not only five centuries, but also the leap into 
-                electronic typesetting, remaining essentially unchanged. It was popularised in 
-                the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-                and more recently with desktop publishing software like Aldus PageMaker 
-                including versions of Lorem Ipsum.</p>
-            </div>
-          </div>
-        
-          <div className='p-4 w-full md:w-2/3 md:order-1'>
-            <div className='p-4 border rounded-md bg-yellow-300'>
-              <h1>Calculator GAE</h1>
-              <UserInput userInput={userInput} onChange={handleChange} />
-            </div>
+export default function GAEPage({ onBackClick }) {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    buydate: new Date('2024-01-01'),
+    todaydate: new Date(),
+    totalday: 0,
+    netCashOut: 0,
+    fxRate: 0,
+    managementFeeRefund: 0,
+    convertToRM: 0,
+    netProfit: 0,
+  });
+
+  const handleChange = (inputIdentifier, newValue) => {
+    setUserInput((prevUserInput) => {
+      const updatedUserInput = {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+
+      // Calculate totalday
+      updatedUserInput.totalday = Math.floor(
+        (new Date() - new Date(updatedUserInput.buydate)) / (1000 * 60 * 60 * 24 *10)
+      );
+
+      // Calculate convertToRM
+      updatedUserInput.convertToRM =
+        (parseFloat(updatedUserInput.netCashOut) +
+        parseFloat(updatedUserInput.managementFeeRefund)) *
+          parseFloat(updatedUserInput.fxRate);
+
+      // Calculate netProfit
+      updatedUserInput.netProfit =
+        updatedUserInput.convertToRM - updatedUserInput.initialInvestment;
+
+      return updatedUserInput;
+    });
+  };
+
+  useEffect(() => {
+    // Fetch live exchange rate from USD to MYR (Malaysian Ringgit)
+    const fetchExchangeRate = async () => {
+      try {
+        const response = await fetch(
+          `https://open.er-api.com/v6/latest/USD`
+        );
+        const data = await response.json();
+
+        // Extract the exchange rate for USD to MYR
+        const usdToMyrRate = data.rates.MYR;
+
+        // Update the state with the fetched exchange rate
+        setUserInput((prevUserInput) => ({
+          ...prevUserInput,
+          fxRate: usdToMyrRate,
+        }));
+      } catch (error) {
+        console.error('Error fetching exchange rate:', error);
+      }
+    };
+    fetchExchangeRate();
+  }, []);
+
+  return (
+    <div className='p-4'>
+      <div className='flex flex-wrap'>
+        <div className='p-4 w-full md:w-1/3 md:order-2 '>
+          <div className='p-7 border rounded-md bg-yellow-300'>
+            <p>GOLD ASSET ENHANCEMENT</p>
+            <br></br>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it to make a type
+              specimen book. It has survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was popularised in
+              the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+              and more recently with desktop publishing software like Aldus PageMaker
+              including versions of Lorem Ipsum.</p>
           </div>
         </div>
-  
-        
-        <button onClick={onBackClick} className='bg-yellow-300 p-4'>Back to Home</button>
+
+        <div className='p-4 w-full md:w-2/3 md:order-1'>
+          <div className='p-4 border rounded-md bg-yellow-300'>
+            <h1 className='p-6 sm:p-5 text-3xl sm:text-7xl font-semibold text-gray-800'>Calculator</h1>
+            <UserInput userInput={userInput} onChange={handleChange} />
+          </div>
+        </div>
       </div>
-    );
-  }
+
+      <button onClick={onBackClick} className='bg-yellow-300 p-4'>Back to Home</button>
+    </div>
+  );
+}
--- src/components/userinput.jsx
+++ src/components/userinput.jsx
@@ -1,52 +1,113 @@
 export default function UserInput({ onChange, userInput }) {
-    return (
-      <section id="user-input">
-        <div className="input-group">
-          <p>
-            <label>Initial Investment</label>
-            <input
-              type="number"
-              required
-              value={userInput.initialInvestment}
-              onChange={(event) =>
-                onChange('initialInvestment', event.target.value)
-              }
-            />
-          </p>
-          <p>
-            <label>Annual Investment</label>
-            <input
-              type="number"
-              required
-              value={userInput.annualInvestment}
-              onChange={(event) =>
-                onChange('annualInvestment', event.target.value)
-              }
-            />
-          </p>
-        </div>
-        <div className="input-group">
-          <p>
-            <label>Expected Return</label>
-            <input
-              type="number"
-              required
-              value={userInput.expectedReturn}
-              onChange={(event) =>
-                onChange('expectedReturn', event.target.value)
-              }
-            />
-          </p>
-          <p>
-            <label>Duration</label>
-            <input
-              type="number"
-              required
-              value={userInput.duration}
-              onChange={(event) => onChange('duration', event.target.value)}
-            />
-          </p>
-        </div>
-      </section>
-    );
-  }
+  return (
+    <section id="user-input">
+      <div className='ml-7'>
+        <p className='mb-2'>
+          <label>Initial Investment</label>
+          <input
+            className='ml-2'
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+              onChange('initialInvestment', event.target.value)
+            }
+          />
+        </p>
+        <p className='mb-2'>
+          <label>Tarikh beli</label>
+          <input
+            className='ml-2'
+            type="date"
+            required
+            value={userInput.buydate}
+            onChange={(event) =>
+              onChange('buydate', event.target.value)
+            }
+          />
+        </p>
+        <p className='mb-2'>
+          <label>Tarikh harini</label>
+          <input
+            className='ml-2'
+            type="date"
+            required
+            value={userInput.todaydate.toISOString().split('T')[0]} // Format the date as YYYY-MM-DD
+            onChange={(event) =>
+              onChange('todaydate', new Date(event.target.value))
+            }
+          />
+        </p>
+        <p className='mb-2'>
+          <label>Jumlah hari</label>
+          <input
+            className='ml-2'
+            type="text" // or "number"
+            readOnly
+            value={userInput.totalday}
+          />
+        </p>
+        <p className='mb-2'>
+          <label>Net Cash Out</label>
+          <input
+            className='ml-2'
+            type="number"
+            required
+            value={userInput.netCashOut}
+            onChange={(event) =>
+              onChange('netCashOut', event.target.value)
+            }
+          />
+        </p>
+        <p className='mb-2'>
+          <label>Fx Rate</label>
+          <input
+            className='ml-2'
+            type="number"
+            required
+            value={userInput.fxRate}
+            onChange={(event) =>
+              onChange('fxRate', event.target.value)
+            }
+          />
+        </p>
+        <p className='mb-2'>
+          <label>Management Fee Refund</label>
+          <input
+            className='ml-2'
+            type="number"
+            required
+            value={userInput.managementFeeRefund}
+            onChange={(event) =>
+              onChange('managementFeeRefund', event.target.value)
+            }
+          />
+        </p>
+        <p className='mb-2'>
+          <label>Conver profit in RM</label>
+          <input
+            className='ml-2'
+            type="number"
+            required
+            value={userInput.convertToRM}
+            onChange={(event) =>
+              onChange('convertToRM', event.target.value)
+            }
+          />
+        </p>
+        <p className='mb-2'>
+          <label>Untung bersih</label>
+          <input
+            className='ml-2'
+            type="number"
+            required
+            value={userInput.netProfit}
+            onChange={(event) =>
+              onChange('netProfit', event.target.value)
+            }
+          />
+        </p>
+      </div>
+    </section>
+  );
+}
