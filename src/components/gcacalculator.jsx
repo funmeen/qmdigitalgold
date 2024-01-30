b2e12ff24242d41a27cee078f45e64aef1add593
funmeen <irfanmuhymeen@gmail.com>
2024-01-31 01:38:15 +0800

    update gca calculator

--- src/components/gcacalculator.jsx
+++ src/components/gcacalculator.jsx
@@ -16,12 +16,14 @@ export default function GcaCalculator({ onChange, userInput }) {
 
     const monthlyData = Array.from({ length: 12 }, (_, month) => {
       const saving = initialInvestment;
+      const savingTrd = initialInvestment * (15 / 100);
       const survive = initialInvestment * (85 / 100);
       const gram = initialInvestment / qmBuy / 1.07;
 
       return {
         month: month + 1,
         saving: saving,
+        savingTrd : savingTrd,
         survive: survive,
         goldPrice: currencyformatter.format(qmBuy),
         gram: gram.toFixed(2),
@@ -31,6 +33,7 @@ export default function GcaCalculator({ onChange, userInput }) {
     const annualReport = {
       month: 'Annual',
       saving: monthlyData.reduce((sum, row) => sum + parseFloat(row.saving), 0).toFixed(0),
+      savingTrd: monthlyData.reduce((sum, row) => sum + parseFloat(row.savingTrd), 0).toFixed(0),
       survive: monthlyData.reduce((sum, row) => sum + parseFloat(row.survive), 0).toFixed(0),
       goldPrice: '',
       gram: monthlyData.reduce((sum, row) => sum + parseFloat(row.gram), 0).toFixed(0),
@@ -47,13 +50,13 @@ export default function GcaCalculator({ onChange, userInput }) {
   return (
     <div>
       <section id="user-input">
-        <div className='ml-7'>
+        <div className='lg:ml-7'>
           <p className='mb-2'>
-            <label className='text-2xl'>Modal (MYR)</label>
+            <label className='text-l lg:text-2xl'>Modal (MYR)</label>
           </p>
           <p>
             <input
-              className='text-2xl mb-4'
+              className='text-l lg:text-2xl mb-4'
               type="number"
               required
               value={userInput.initialInvestment}
@@ -64,11 +67,11 @@ export default function GcaCalculator({ onChange, userInput }) {
           </p>
           
           <p className='mb-2'>
-            <label className='text-2xl'>QM Buy (MYR)</label>
+            <label className='text-l lg:text-2xl'>QM Buy (MYR)</label>
           </p>
           <p>
             <input
-              className='text-2xl mb-4'
+              className='text-l lg:text-2xl mb-4'
               type="number"
               required
               value={userInput.qmBuy}
@@ -79,11 +82,11 @@ export default function GcaCalculator({ onChange, userInput }) {
           </p>
 
           <p className='mb-2'>
-            <label className='text-2xl'>Kenaikan Harga Emas</label>
+            <label className='text-l lg:text-2xl'>Kenaikan Harga Emas</label>
           </p>
           <p>
             <input
-              className='text-2xl mb-6'
+              className='text-l lg:text-2xl mb-6'
               type="number"
               required
               value={userInput.priceChanges}
@@ -95,30 +98,116 @@ export default function GcaCalculator({ onChange, userInput }) {
           
         </div>
       </section>
-        <div className='flex-col'>
-            <table className='mx-auto border-collapse border border-black'>
-                <thead>
-                    <tr>
-                    <th className='text-center text-xl pl-1 pr-1 border border-black'>Month</th>
-                    <th className='text-center text-xl pl-1 pr-1 border border-black'>Saving</th>
-                    <th className='text-center text-xl pl-1 pr-1 border border-black'>Survive</th>
-                    <th className='text-center text-xl pl-1 pr-1 border border-black'>Gold Price</th>
-                    <th className='text-center text-xl pl-1 pr-1 border border-black'>Gram</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {tableData.map((rowData, index) => (
-                    <tr key={index}>
-                        <td className='text-center border border-black'>{rowData.month}</td>
-                        <td className='text-center pl-1 border border-black'>{currencyformatter.format(rowData.saving)}</td>
-                        <td className='text-center pl-1 border border-black'>{currencyformatter.format(rowData.survive)}</td>
-                        <td className='text-center pl-1 border border-black'>{rowData.goldPrice}</td>
-                        <td className='text-center pl-1 border border-black'>{rowData.gram} g</td>
-                    </tr>
-                    ))}
-                </tbody>
-            </table>
+      {/* Monthly Table Result */}
+      <section>
+        <div className='mx-auto flex flex-col xl:flex-row'>
+          <div className='w-full mr-5 xl:w-1/2 h-auto'>
+            <div className='flex overflow-x-auto space-x-8'>
+              <table className='mx-auto border border-black bg-white'>
+                  <thead>
+                      <tr>
+                      <th className='text-center text-l lg:text-xl pl-1 pr-1 border border-black'>Month</th>
+                      <th className='text-center text-l lg:text-xl pl-1 pr-1 border border-black'>Income</th>
+                      <th className='text-center text-l lg:text-xl pl-1 pr-1 border border-black'>Saving</th>
+                      <th className='text-center text-l lg:text-xl pl-1 pr-1 border border-black'>Survive</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      {tableData.map((rowData, index) => (
+                      <tr key={index}>
+                          <td className='text-center text-m border border-black'>{rowData.month}</td>
+                          <td className='text-center pl-1 border border-black'>{currencyformatter.format(rowData.saving)}</td>
+                          <td className='text-center pl-1 border border-black'>{currencyformatter.format(rowData.savingTrd)}</td>
+                          <td className='text-center pl-1 border border-black'>{currencyformatter.format(rowData.survive)}</td>
+                      </tr>
+                      ))}
+                  </tbody>
+              </table>
+            </div>
+          </div>
+          <div className='w-full xl:w-1/2 mt-5 pr-4 xl:mt-0 h-auto'>
+            <div className='flex overflow-x-auto space-x-8'>
+              <table className='mx-auto border border-black bg-white'>
+                  <thead>
+                      <tr>
+                      <th className='text-center text-l lg:text-xl pl-1 pr-1 border border-black'>Month</th>
+                      <th className='text-center text-l lg:text-xl pl-1 pr-1 border border-black'>Saving</th>
+                      <th className='text-center text-l lg:text-xl pl-1 pr-1 border border-black'>Survive</th>
+                      <th className='text-center text-l lg:text-xl pl-1 pr-1 border border-black'>Gold Price</th>
+                      <th className='text-center text-l lg:text-xl pl-1 pr-1 border border-black'>Gram</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      {tableData.map((rowData, index) => (
+                      <tr key={index}>
+                          <td className='text-center text-m border border-black'>{rowData.month}</td>
+                          <td className='text-center pl-1 border border-black'>{currencyformatter.format(rowData.saving)}</td>
+                          <td className='text-center pl-1 border border-black'>{currencyformatter.format(rowData.survive)}</td>
+                          <td className='text-center pl-1 border border-black'>{rowData.goldPrice}</td>
+                          <td className='text-center pl-1 border border-black'>{rowData.gram} g</td>
+                      </tr>
+                      ))}
+                  </tbody>
+              </table>
+            </div>
+          </div>
         </div>
+      </section>
+      {/* Monthly Table Result */}
+      <section className='mt-5'>
+        <div className='mx-auto flex flex-col xl:flex-row'>
+          <div className='w-full mr-5 xl:w-1/2 h-auto'>
+            <div className='flex overflow-x-auto space-x-8'>
+              <table className='mx-auto border border-black bg-white'>
+                  <thead>
+                      <tr>
+                      <th className='text-center text-l lg:text-xl pl-1 pr-1 border border-black'>Year</th>
+                      <th className='text-center text-l lg:text-xl pl-1 pr-1 border border-black'>Simpanan</th>
+                      <th className='text-center text-l lg:text-xl pl-1 pr-1 border border-black'>Dividen</th>
+                      <th className='text-center text-l lg:text-xl pl-1 pr-1 border border-black'>Total</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      {tableData.map((rowData, index) => (
+                      <tr key={index}>
+                          <td className='text-center text-m border border-black'>{rowData.month}</td>
+                          <td className='text-center pl-1 border border-black'>{currencyformatter.format(rowData.saving)}</td>
+                          <td className='text-center pl-1 border border-black'>{currencyformatter.format(rowData.savingTrd)}</td>
+                          <td className='text-center pl-1 border border-black'>{currencyformatter.format(rowData.survive)}</td>
+                      </tr>
+                      ))}
+                  </tbody>
+              </table>
+            </div>
+          </div>
+          <div className='w-full xl:w-1/2 mt-5 pr-4 xl:mt-0 h-auto'>
+            <div className='flex overflow-x-auto space-x-8'>
+              <table className='mx-auto border border-black bg-white'>
+                  <thead>
+                      <tr>
+                      <th className='text-center text-l lg:text-xl pl-1 pr-1 border border-black'>Month</th>
+                      <th className='text-center text-l lg:text-xl pl-1 pr-1 border border-black'>Saving</th>
+                      <th className='text-center text-l lg:text-xl pl-1 pr-1 border border-black'>Survive</th>
+                      <th className='text-center text-l lg:text-xl pl-1 pr-1 border border-black'>Gold Price</th>
+                      <th className='text-center text-l lg:text-xl pl-1 pr-1 border border-black'>Gram</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      {tableData.map((rowData, index) => (
+                      <tr key={index}>
+                          <td className='text-center text-m border border-black'>{rowData.month}</td>
+                          <td className='text-center pl-1 border border-black'>{currencyformatter.format(rowData.saving)}</td>
+                          <td className='text-center pl-1 border border-black'>{currencyformatter.format(rowData.survive)}</td>
+                          <td className='text-center pl-1 border border-black'>{rowData.goldPrice}</td>
+                          <td className='text-center pl-1 border border-black'>{rowData.gram} g</td>
+                      </tr>
+                      ))}
+                  </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
